6efd59fd375531788617fca4f4fc8ec0edcf5705
Sukriti Srivastava <79142553+sukscode@users.noreply.github.com>
2022-02-12 18:40:01 +0530

    till challenge 12

--- app.js
+++ app.js
@@ -15,28 +15,53 @@ const contactContent =
 
 const app = express();
 
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+let posts=[];
 
 //Home
 app.get("/", function (req, res) {
-  res.render("home");
-  res.render("home", { startingContent: homeStartingContent }); //key:value
+  //res.render("home");
+  
+  return res.render("home", { 
+    starting: homeStartingContent,
+    posts:posts
+   });
+  
+   //key:value
+  
 });
 
 //About
 app.get("/about", function (req, res) {
-  res.render("about");
-  res.render("about", { content: aboutContent });
+  //res.render("about");
+  return res.render("about", { content: aboutContent });
 });
 
 //Contact
 app.get("/contact", function (req, res) {
-  res.render("contact");
-  res.render("contact", { contact: contactContent });
+  //res.render("contact");
+  return res.render("contact", { contact: contactContent });
 });
+
+//Compose
+app.get("/compose", function (req, res) {
+  return res.render("compose");
+});
+
+app.post("/compose",function(req,res){
+  const post={
+    title: req.body.postTitle,
+    content: req.body.text
+  };
+  posts.push(post);
+  res.redirect("/");
+ 
+})
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
